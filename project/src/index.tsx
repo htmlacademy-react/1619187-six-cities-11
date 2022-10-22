395b535fdbee1d8637c52b46313f9ccf9218084a
Yana <yana.timofeeva.94@mail.ru>
2022-10-22 16:35:20 +0400

    Создала компоненты для всех страниц приложения, одну карточку предложения выделила в отд компонент

--- project/src/index.tsx
+++ project/src/index.tsx
@@ -1,6 +1,12 @@
+//входной файл для старта работы приложения
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './components/app/app';
+
+const Setting = { //enum- перечисление в виде объекта. В результате компиляции получится обычный JavaScript-объект.
+  PlaceCardCount: 5,
+} as const;
+
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
@@ -9,6 +15,6 @@ const root = ReactDOM.createRoot(
 
 root.render(
   <React.StrictMode>
-    <App />
+    <App placeCardCount= {Setting.PlaceCardCount}/>
   </React.StrictMode>
 );
